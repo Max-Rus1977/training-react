a4a21ed56ed4376f338352ef520f9fb483ae7b9e
Max-Rus1977 <maxsvet77@yandex.ru>
2022-12-05 17:18:50 +0300

    Приложение начинается с домашней страницы HomePage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
-import StartPage from './pages/StartPage';
 import HomePage from './pages/HomePage'
 import PostPages from './pages/PostPages'
 import About from './pages/About';
@@ -14,7 +13,7 @@ const router = createBrowserRouter([
   {
     path: '/', element: <App />, errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <StartPage />, errorElement: <ErrorPage />, },
+      { index: true, element: <HomePage />, errorElement: <ErrorPage />, },
       { path: '/home', element: <HomePage />, errorElement: <ErrorPage />, },
       { path: '/posts', element: <PostPages />, errorElement: <ErrorPage />, },
       { path: '/posts/:id', element: <OnePost />, errorElement: <ErrorPage /> },
