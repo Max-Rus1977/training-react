e829b715725aa66e7901a519a50c48112a979b0e
Max-Rus1977 <maxsvet77@yandex.ru>
2022-12-05 17:15:26 +0300

    Приложение начинается со стартовой страницы заглушки StartPage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import StartPage from './pages/StartPage';
 import HomePage from './pages/HomePage'
 import PostPages from './pages/PostPages'
 import About from './pages/About';
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
   {
     path: '/', element: <App />, errorElement: <ErrorPage />,
     children: [
+      { index: true, element: <StartPage />, errorElement: <ErrorPage />, },
       { path: '/home', element: <HomePage />, errorElement: <ErrorPage />, },
       { path: '/posts', element: <PostPages />, errorElement: <ErrorPage />, },
       { path: '/posts/:id', element: <OnePost />, errorElement: <ErrorPage /> },
